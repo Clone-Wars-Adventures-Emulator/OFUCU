c7de649fe5ee6d11384a0213dd9585113c731fac
Joseph <kartingman25@gmail.com>
2023-06-16 11:21:31 -0400

    Fix Incorrect Rect calculations

--- Editor/Flash/Records/Rect.cs
+++ Editor/Flash/Records/Rect.cs
@@ -2,23 +2,23 @@ namespace CWAEmu.FlashConverter.Flash.Records {
     public class Rect {
         private int nBits;
         public int ByteLength => (5 + nBits * 4) / 8 + 1;
-        public float X => XTwips / 20.0f;
-        public float Y => YTwips / 20.0f;
-        public float Width => WidthTwips / 20.0f;
-        public float Height => HeightTwips / 20.0f;
-        public int XTwips { get; private set; }
-        public int YTwips { get; private set; }
-        public int WidthTwips { get; private set; }
-        public int HeightTwips { get; private set; }
+        public float X => XMinTwips / 20.0f;
+        public float Y => YMinTwips / 20.0f;
+        public float Width => Math.Abs(XMaxTwips - XMinTwips) / 20.0f;
+        public float Height => Math.Abs(YMaxTwips - YMinTwips) / 20.0f;
+        public int XMinTwips { get; private set; }
+        public int YMinTwips { get; private set; }
+        public int XMaxTwips { get; private set; }
+        public int YMaxTwips { get; private set; }
 
         public static Rect readRect(Reader reader) {
             Rect rect = new();
 
             rect.nBits = (int) reader.readUBits(5);
-            rect.XTwips = reader.readBits(rect.nBits);
-            rect.WidthTwips = reader.readBits(rect.nBits);
-            rect.YTwips = reader.readBits(rect.nBits);
-            rect.HeightTwips = reader.readBits(rect.nBits);
+            rect.XMinTwips = reader.readBits(rect.nBits);
+            rect.XMaxTwips = reader.readBits(rect.nBits);
+            rect.YMinTwips = reader.readBits(rect.nBits);
+            rect.YMaxTwips = reader.readBits(rect.nBits);
 
             reader.endBitRead();
 
